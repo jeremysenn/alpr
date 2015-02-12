caa8389ba0a002b700920df45583aac2a5d75d53
Jeremy Senn <senn.jeremy@gmail.com>
2015-02-12 09:33:02 -0800

    alpr json results

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,7 +1,7 @@
 class Scan < ActiveRecord::Base
   
   def alpr_scan(path_to_file)
-    self.results = `alpr #{path_to_file}`
+    self.results = `alpr -j #{path_to_file}`
     self.save
   end
 end

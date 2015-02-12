2eb95d99ccdca0d742db213ad19d3599ada4cfbf
Jeremy Senn <senn.jeremy@gmail.com>
2015-02-12 10:01:37 -0800

    alpr json results

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,7 +1,15 @@
 class Scan < ActiveRecord::Base
   
   def alpr_scan(path_to_file)
-    self.results = `alpr -j #{path_to_file}`
+    JSON.parse(`alpr -j #{path_to_file}`)
+  end
+  
+  def plate_number
+    results["results"].first["plate"] unless results.blank
+  end
+  
+  def save_alpr_scan(path_to_file)
+    self.results = JSON.parse(`alpr -j #{path_to_file}`)
     self.save
   end
 end
